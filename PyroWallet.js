9ab2cca92d370979988777445ef1630c965a9c02
crypt04dvisor <36528905+crypt04dvisor@users.noreply.github.com>
2020-02-29 14:26:20 -0600

    Update PyroWallet.js

--- PyroWallet.js
+++ PyroWallet.js
@@ -449,7 +449,7 @@ function recalc() {
 
 	function getmsg() {
 
-		return "dashboard.html \r\n\r\nEthereum address: "+pwDerivedKey+"  \r\nMnemonic Phrase: '"+localStorage.getItem("d12keys")+"'\r\n\r\n(HD derivation path is m/0'/0'/0')\r\n\r\nHow to access tokens and ethers: \r\n1. Go here https://www.myetherwallet.com/#send-transaction \r\n2. Select 'Mnemonic Phrase'\r\n3. Insert Phrase and set derivation path is m/0'/0'/0' \r\n3. Click 'Unlock' \r\n4. Add custom token > "+erc20contract_address+" (PYRO,18)";
+		return "dashboard.html \r\n\r\nEthereum address: "+openkey+"  \r\nMnemonic Phrase: '"+localStorage.getItem("d12keys")+"'\r\n\r\n(HD derivation path is m/0'/0'/0')\r\n\r\nHow to access tokens and ethers: \r\n1. Go here https://www.myetherwallet.com/#send-transaction \r\n2. Select 'Mnemonic Phrase'\r\n3. Insert Phrase and set derivation path is m/0'/0'/0' \r\n3. Click 'Unlock' \r\n4. Add custom token > "+erc20contract_address+" (PYRO,18)";
 
 	}
 
